51383e8d55f8bbf8035a1eefee9a9983b68200e5
Jorge <xxopxe@gmail.com>
2012-09-04 12:00:03 -0300

    Support for OpenMoko Accelerometers under SHR-testing.

--- deviceloaders/accelopenmoko.lua
+++ deviceloaders/accelopenmoko.lua
@@ -1,114 +1,160 @@
---- Library for accesing the accelerometer of a XO 1.75.
--- The device will be named "accelxo", module "accelxo". 
--- @module accelxo
+--- Library for accesing the accelerometer of a OpenMoko.
+-- This module starts two devices, one for each one of the acceleration
+-- sensors of a OpenMoko FreeRunner smartphone. Runs under SHR-testing OS.
+-- The module of both devices is "accelopenmoko".
+-- The acceleration is measured in mg (1/1000th of earth gravity).
+-- The first Device is named "accelerometer.1" and the axes are, when 
+-- looking from the front and the phone laying on a desk:
+-- x (horizontal to the right and away), y (horizontal to the left and away) 
+-- and z (down).
+-- The second Device is named "accelerometer.2" and the axes are, when 
+-- looking from the front and the phone laying on a desk:
+-- x (horizontal to the right), y (horizontal and away) 
+-- and z (down).
+-- @usage local accel = toribio.wait_for_device('accelerometer.1')
+--sched.sigrun(
+--    {emitter=accel.task, events={accel.events.data}},
+--    function(_,_,x,y,z) print (x,y,z) end
+--)
+-- @module accelopenmoko
 -- @alias device
 
 local M = {}
 
-M.init = function()
-	toribio = require 'toribio'
+M.init = function(conf)
+	local toribio = require 'toribio'
+	local nixio = require 'nixio'
+	local sched = require 'sched'
 
-	local sysfs1 = '/sys/devices/platform/lis302dl.1'
-	local sysfs1_sample_rate = sysfs1 .. '/sample_rate'
-	local sysfs1_threshold = sysfs1 .. '/threshold'
-	local stream1=assert(io.open('/dev/input/event3', 'rb'))
+	--local sysfs1 = '/sys/devices/platform/lis302dl.1'
+	local sysfs1 = '/sys/class/i2c-adapter/i2c-0/0-0073/lis302dl.1'
+	local stream1=assert(io.open('/dev/input/event2', 'rb'))
+	--local stream1 = assert(nixio.open('/dev/input/event2', nixio.open_flags('rdonly', 'nonblock')))
 
-	local sysfs2 = '/sys/devices/platform/lis302dl.2'
-	local sysfs2_sample_rate = sysfs2 .. '/sample_rate'
-	local sysfs2_threshold = sysfs2 .. '/threshold'
-	local stream2=assert(io.open('/dev/input/event4', 'rb'))
+
+	--local sysfs2 = '/sys/devices/platform/lis302dl.2'
+	local sysfs2 = '/sys/class/i2c-adapter/i2c-0/0-0073/lis302dl.2'
+	local stream2=assert(io.open('/dev/input/event3', 'rb'))
+	--local stream2 = assert(nixio.open('/dev/input/event3', nixio.open_flags('rdonly', 'nonblock')))
+
 	
-	local get_accel = function(stream)
-		local x, y, z
-		repeat
-			local event = stream:read(16)
-			--local time=message:sub(1, 4)
-			local etype = event:byte(10) -- only last byte
-			local ecode = event:byte(12) -- only last byte
-			if etype==1 or etype==2 then
-				local evalue = event:byte(13, 14) --2 bytes (~65.5 g)
-				local value = 256*evalue[1] + evalue[2]
-				if ecode==0 then x=value 
-				elseif ecode==1 then y=value 
-				elseif ecode==2 then z=value end
-			--elseif etype==0 and ecode==0 then
-			--	return x, y, z
+			--- Read the acceleration from sensor 1.
+			-- The acceleration is measured in mg (1/1000th of earth gravity)
+			-- the axis are, when looking from the front and the phone laying on a desk:
+			-- x (horizontal to the right and away), y (horizontal to the left and away) 
+			-- and z (down)
+			-- @return The x, y and z magnitudes.
+
+			--- Read the acceleration from sensor 2.
+			-- The acceleration is measured in mg (1/1000th of earth gravity)
+			-- the axis are, when looking from the front and the phone laying on a desk:
+			-- x (horizontal to the right), y (horizontal and away) 
+			-- and z (down)
+			-- @return The x, y and z magnitudes.
+
+		
+	local function build_device(name, sysfs, stream, event_accel)
+
+		local sysfs_sample_rate = sysfs .. '/sample_rate'
+		local sysfs_threshold = sysfs .. '/threshold'
+		
+		local delay_read = conf.delay or 1
+		local task_read = sched.new_task(function()
+				local monitor_accel = function(x, y, z)
+					for _=1,10 do
+						local event = assert(stream:read(16))
+						--local time=message:sub(1, 4)
+						local etype = event:byte(9) -- only last byte
+						local ecode = event:byte(11) -- only last byte
+			--			print (etype,ecode)
+						if etype==3 or etype==2 then
+							local value = event:byte(13) + 256*event:byte(14)--2 bytes (~65.5 g)
+							if value>32768 then value=value-0xFFFF end
+			--			print ('',value)
+							if ecode==0 then x=value 
+							elseif ecode==1 then y=value 
+							elseif ecode==2 then z=value end
+						elseif etype==0 and ecode==0 then
+							return x, y, z
+						end
+					end
+					error('Accelerator sensor fails to sync: '..name)
+				end
+				local x, y, z
+				
+				repeat
+					x,y,z = monitor_accel(x, y, z)
+				until x and y and z
+				while true do
+					x,y,z = monitor_accel(x, y, z)
+					sched.signal(event_accel,x,y,z)
+					sched.sleep(delay_read)
+				end
+
+			end)
+
+		local device={
+			--- Name of the device.
+			-- Either 'accelerometer.1' or 'accelerometer.2'
+			name=name,
+			
+			--- Module name.
+			-- In this case, "accelopenmoko".
+			module="accelopenmoko",
+			
+			--- Sensor data events.
+			-- The events that the sensor can emit.
+			-- @field data A data signal, with x,y,z acceleration values as parameters.
+			events={
+				data=event_accel,
+			},
+			
+			--- Set the sample rate for sensor.
+			-- This is the intarnal sample rate, and the known supported values are
+			-- 100 and 400 Hz.
+			-- @param hz The rate in hz.
+			set_rate = function(hz)
+				local f=io.open(sysfs_sample_rate, 'w')
+				if not f then return end
+				f.write(hz..'\n')
+				f.close()
+			end,
+			
+			--- Set the threshold for sensor.
+			-- Values around 10 or 18 are usual.
+			-- @param threshold The threshold value 
+			set_threshold = function(threshold)
+				local f=io.open(sysfs_threshold, 'w')
+				if not f then return end
+				f.write(threshold..'\n')
+				f.close()
+			end,
+			
+			--- Sensor task.
+			-- This is the task that will emit data signals.
+			task = task_read,
+			
+			--- Enable sensing.
+			-- This allows to enable and disable the sensing.
+			-- @param enable boolean indicating if the sensor data events must be generated.
+			-- @param delay time delay between events. If omitted, a configuration value 
+			-- is used (field _deviceloaders.accelopenmoko.delay_), 1 otherwise.
+			run = function(enable, delay)
+				delay_read = delay or delay_read
+				task_read:set_pause(not enable)
 			end
-		until x and y and z
-		return x, y, z
+		}
+		return device
 	end
 	
-	local device={
-		name="accelopenmoko",
-		module="accelopenmoko",
-		
-		--- Read the acceleration from sensor 1.
-		-- The acceleration is measured in mg (1/1000th of earth gravity)
-		-- the axis are, when looking from the front and the phone laying on a desk:
-		-- x (horizontal to the right and away), y (horizontal to the left and away) 
-		-- and z (down)
-		-- @return The x, y and z magnitudes.
-		get_accel1 = function()
-			return get_accel(stream1)
-		end,
+	local device1=build_device('accelerometer.1', sysfs1, stream1, {})
+	_G.debugprint('device object created', device1.name)
+	toribio.add_device(device1)
+	
+	local device2=build_device('accelerometer.2', sysfs2, stream2, {})
+	_G.debugprint('device object created', device2.name)
+	toribio.add_device(device2)
 
-		--- Read the acceleration from sensor 2.
-		-- The acceleration is measured in mg (1/1000th of earth gravity)
-		-- the axis are, when looking from the front and the phone laying on a desk:
-		-- x (horizontal to the right), y (horizontal and away) 
-		-- and z (down)
-		-- @return The x, y and z magnitudes.
-		get_accel2 = function()
-			return get_accel(stream2)
-		end,		
-		
-		--- Set the sample rate for sensor 1.
-		-- This is the intarnal sample rate, and the known supported values are
-		-- 100 and 400 Hz.
-		-- @param hz The rate in hz.
-		set_rate1 = function(hz)
-			local f=io.open(sysfs1_sample_rate, 'w')
-			if not f then return end
-			f.write(hz..'\n')
-			f.close()
-		end,
-
-		--- Set the sample rate for sensor 2.
-		-- This is the intarnal sample rate, and the known supported values are
-		-- 100 and 400 Hz.
-		-- @param hz The rate in hz.
-		set_rate1 = function(hz)
-			local f=io.open(sysfs2_sample_rate, 'w')
-			if not f then return end
-			f.write(hz..'\n')
-			f.close()
-		end,
-
-		
-		--- Set the threshold for sensor 1.
-		-- Values around 10 or 18 are usual.
-		-- @param threshold The threshold value 
-		set_threshold1 = function(threshold)
-			local f=io.open(sysfs1_threshold, 'w')
-			if not f then return end
-			f.write(threshold..'\n')
-			f.close()
-		end,
-
-		--- Set the threshold for sensor 2.
-		-- Values around 10 or 18 are usual.
-		-- @param threshold The threshold value 
-		set_threshold1 = function(threshold)
-			local f=io.open(sysfs2_threshold, 'w')
-			if not f then return end
-			f.write(threshold..'\n')
-			f.close()
-		end,
-		
-	}
-
-	_G.debugprint('device object created', device.name)
-	toribio.add_device(device)
 end
 
 return M
