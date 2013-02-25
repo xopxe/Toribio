505908ecddd2ccc5ef682b8c4c2156da77783018
Jorge <xxopxe@gmail.com>
2013-02-25 16:52:12 -0200

    aligned to new lback behavior

--- tasks/listener_bblback.lua
+++ tasks/listener_bblback.lua
@@ -5,13 +5,13 @@ M.init = function()
 	
 	return sched.run(function()
 		local toribio = require 'toribio'
-		local lback = assert(toribio.wait_for_device('bb-lback', 5))
+		local lback = assert(toribio.wait_for_device('bb-lback'))
 		
 		while true do
-			lback.send(sched.get_time())
-			sched.sleep(5)
-			print(lback.read())
-			--sched.sleep(1)
+			print(lback.send(sched.get_time()))
+			--sched.sleep(5)
+			--print(lback.read())
+			sched.sleep(1)
 			sched.yield()
 		end
 	end)
