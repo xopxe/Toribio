9da48aa31a2aa00b8db4444704464f6963a75603
Jorge <xxopxe@gmail.com>
2013-04-04 15:24:06 -0300

    sync for Lumen, fixes on parsing

--- deviceloaders/rnr_client.lua
+++ deviceloaders/rnr_client.lua
@@ -14,25 +14,10 @@ M.init = function(conf)
 	local sched = require 'sched'
 	local log = require 'log'
 	
+	local notification_arrival = {} --signal
+	
 	local ip = conf.ip or '127.0.0.1'
 	local port = conf.port or 8182
-	
-	local device={
-		--- Name of the device (in this case, 'gpsd').
-		name = 'rnr_client', 
-		
-		--- Module name (in this case, 'gpsd').
-		module = 'rnr_client', 
-		
-		--- Task that will emit signals associated to this device.
-		task = selector.task,  
-		
-		--- Events emitted by this device.
-		-- @field notification_arrival a new notification has arrived.  The first parameter is a table with the notifications content.
-		events = { 
-			notification_arrival = {}
-		},
-	}
 	
 	local function parse_params(data)
 		local params={}
@@ -43,17 +28,7 @@ M.init = function(conf)
 				params[k]=v
 			end
 		end
-
-		--[[
-		if configuration.use_sha1 then
-			local signstatus=sign_message(params)
-			print("SHA1 signature: ", signstatus)
-			if signstatus~='ok' then 
-				print("WARN: Purging message (signature check failure)")
-				params={} 
-			end
-		end
-		--]]
+		
 		for k, v in pairs(params) do
 			params[k]=v
 		end
@@ -71,24 +46,48 @@ M.init = function(conf)
 			elseif line == 'END' then
 				if notification_lines then
 					local notification = parse_params(notification_lines)
-					sched.signal(device.events.notification_arrival, notification)
+					sched.signal(notification_arrival, notification)
 					notification_lines = nil
 				end
-			else
+			elseif notification_lines then
 				notification_lines[#notification_lines+1]=line
 			end
-	
+			
 			return true
 		end
 	end
-	local skt = selector.new_tcp_client(ip, port, nil, nil, 'line', get_incomming_handler())
-	device.skt = skt
+	
+	local skt, err = selector.new_tcp_client(ip, port, nil, nil, 'line', get_incomming_handler())
+	if not skt then 
+		log('RNRCLIENT', 'ERROR', 'Failed to connect to %s:%s with error: %s', tostring(ip), tostring(port),tostring(err))
+		return
+	end
+	
+	local device={
+		--- Name of the device (in this case, 'gpsd').
+		name = 'rnr_client', 
+		
+		--- Module name (in this case, 'gpsd').
+		module = 'rnr_client', 
+		
+		--- Task that will emit signals associated to this device.
+		task = selector.task,  
+		
+		--- Events emitted by this device.
+		-- @field notification_arrival a new notification has arrived.  The first parameter is a table with the notifications content.
+		events = { 
+			notification_arrival = notification_arrival
+		},
+		
+		skt = skt
+	}
 	
 	--- Add a Subscription.
 	-- When subscribed, matching notification will arrive as signals (see @{events})
 	-- @param subscrib_id a unique subscription id. If nil, a random one will be generated.
 	-- @param filter an array contaning the subscription filter. Each entry in the array is a table
 	-- containing 'attr', 'op' and 'value' fields describing an expression.
+	-- @return _true_ on succes, _nil, err_ on failure
 	-- @usage local rnr = bobot.wait_for_device('rnr_client')
 	--rnr.subscribe( 'subscrib100', {
 	--	{attrib='sensor', op='=', value='node1'},
@@ -102,31 +101,32 @@ M.init = function(conf)
 		end
 		vlines[#vlines+1]= 'END\n'
 		local s = table.concat(vlines, '\n')
-		skt.send_sync(s)
+		return skt:send_sync(s)
 	end
 
 	--- Remove a Subscription.
 	-- @param subscrib_id a unique subscription id.
+	-- @return _true_ on succes, _nil, err_ on failure
 	device.unsubscribe = function (subscrib_id) 
 		local s ='UNSUBSCRIBE\nsubscription_id='..subscrib_id.. '\nEND\n'
-		skt.send_sync(s)
+		return skt:send_sync(s)
 	end
 	
 	
 	--- Emit a Notification.
-	-- @param notif_id a unique notification id. If nil, a random one will be generated.
 	-- @param data a table with the data to be sent.
+	-- @return _true_ on succes, _nil, err_ on failure
 	-- @usage local rnr = bobot.wait_for_device('rnr_client')
 	--rnr.subscribe( 'notif100', {sensor = 'node2', temperature = 25} )
-	device.emit_notification = function (notif_id, data)
-		notif_id = notif_id or tostring(math.random(2^30))
-		local vlines={[1]='NOTIFICATION', [2]='notification_id='..notif_id}
+	device.emit_notification = function (data)
+		data.notif_id = data.notif_id or tostring(math.random(2^30))
+		local vlines={[1]='NOTIFICATION'}
 		for k, v in pairs(data) do
 			vlines[#vlines+1]= tostring(k) .. '=' .. tostring(v)
 		end
 		vlines[#vlines+1]= 'END\n'
 		local s = table.concat(vlines, '\n')
-		skt.send_sync(s)
+		return skt:send_sync(s)
 	end
 	
 	log('RNRCLIENT', 'INFO', 'Device %s created: %s', device.module, device.name)
