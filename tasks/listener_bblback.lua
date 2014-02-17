fa7094504587604d5b57dcd42aa91bb4e30021ad
Jorge <xxopxe@gmail.com>
2014-02-17 16:41:55 -0200

    bobot testing

--- tasks/listener_bblback.lua
+++ tasks/listener_bblback.lua
@@ -1,17 +1,27 @@
+local sched = require 'sched'
+local toribio = require 'toribio'
+
 local M = {}
 
-M.init = function()
-	local sched = require 'sched'
-	
-	return sched.run(function()
-		local toribio = require 'toribio'
+M.init = function()	
+	sched.run(function()
 		local lback = assert(toribio.wait_for_device('bb-lback'))
 		
 		while true do
 			print(lback.send(sched.get_time()))
-			--sched.sleep(5)
-			--print(lback.read())
 			sched.sleep(1)
+		end
+	end)
+	sched.run(function()
+		local button = assert(toribio.wait_for_device({module='bb-button'}))
+		local v=button.getValue()
+		while true do
+      local vnew=button.getValue()
+      if vnew~=v then 
+        v=vnew
+        print('button:',v)
+      end
+ 			sched.sleep(0.1)
 		end
 	end)
 end
